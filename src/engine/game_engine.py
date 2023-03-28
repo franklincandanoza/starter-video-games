337f2d458f082e3ff8799357e01fd0ef931d0b49
Franklin Candanoza <franklin.candanoza@bold.co>
2023-03-28 14:40:43 -0500

    Moviendo el rectángulo sin salirse de la pantalla

--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -60,6 +60,28 @@ class GameEngine:
         # Avanzamos en X a 100 pixeles por segundo (Delta time)
         self.pos_cuad.x += self.vel_cuad.x * self.delta_time
         self.pos_cuad.y += self.vel_cuad.y * self.delta_time
+        
+        # Necesitamos validar si nos estamos saliendo del límite de la pantalla
+        # Rectángulo de la pantalla
+        screen_rect = self.screen.get_rect()
+        
+        # Réctangulo del cuadrado
+        cuad_rect = self.surf_cuad.get_rect(topleft=self.pos_cuad)
+        
+        if cuad_rect.left <=0 or cuad_rect.right >= screen_rect.width:
+            # Invertimos la velocidad
+            self.vel_cuad.x *= -1
+            
+            # Obtenemos un rectángulo que esté dentro del límite de otro rectángulo
+            cuad_rect.clamp_ip(screen_rect)
+            self.pos_cuad.x = cuad_rect.x
+            
+        if cuad_rect.top <=0 or cuad_rect.bottom >= screen_rect.height:
+            # Invertimos la velocidad
+            self.vel_cuad.y *= -1
+            cuad_rect.clamp_ip(screen_rect)
+            self.pos_cuad.y = cuad_rect.y
+        
 
     def _draw(self):
         # Limpiamos la pantalla
